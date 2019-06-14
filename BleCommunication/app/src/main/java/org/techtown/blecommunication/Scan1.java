d1b71919e505ab964011ea161cbcb9c67e23e0e9
gmldbd94 <gmldbd@likelion.org>
2019-06-14 13:59:43 +0900

    12

--- BleCommunication/app/src/main/java/org/techtown/blecommunication/Scan1.java
+++ BleCommunication/app/src/main/java/org/techtown/blecommunication/Scan1.java
@@ -10,25 +10,18 @@ import android.bluetooth.le.ScanFilter;
 import android.bluetooth.le.ScanResult;
 import android.bluetooth.le.ScanSettings;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
-import android.location.Address;
-import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.nfc.Tag;
+
 import android.os.AsyncTask;
 import android.os.Build;
-import android.os.Looper;
 import android.os.ParcelUuid;
-import android.support.annotation.NonNull;
-//import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -36,27 +29,15 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationCallback;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationResult;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.location.LocationSettingsRequest;
-import com.google.android.gms.maps.model.LatLng;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-
-import java.io.IOException;
-import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Locale;
-import java.util.Set;
 import java.util.Vector;
 
 public class Scan1 extends AppCompatActivity {
@@ -87,28 +68,24 @@ public class Scan1 extends AppCompatActivity {
     int check = 0;
     String isSOS;
 
-    //location을 위한
-    private FusedLocationProviderClient mFusedLocationClient;
-    private LocationRequest locationRequest;
-    private Location location;
-    String[] REQUIRED_PERMISSIONS = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};  // 외부 저장소
-    private static final int GPS_ENABLE_REQUEST_CODE = 2001;
-    private static final int UPDATE_INTERVAL_MS = 5000;  // 5초
-    private static final int FASTEST_UPDATE_INTERVAL_MS = 2000; // 2초
-    Location mCurrentLocatiion;
-    LatLng currentPosition;
-    private static final int PERMISSIONS_REQUEST_CODE = 100;
-    boolean needRequest = false;
-    String TAG = "testing";
-
     ArrayList<String> SOS_id = new ArrayList<>();
     ArrayList<Double> SOS_distance = new ArrayList<>();
 
-    double longitude;
-    double latitude;
 
     Button senddt_bt;
     Button show_bt;
+
+    //위치
+    private GpsInfo gps;
+    private final int PERMISSIONS_ACCESS_FINE_LOCATION = 1000;
+    private final int PERMISSIONS_ACCESS_COARSE_LOCATION = 1001;
+    private boolean isAccessFineLocation = false;
+    private boolean isAccessCoarseLocation = false;
+    private boolean isPermission = false;
+    double latitude;
+    double longitude;
+
+
 
     //firebase 전송을 위한 세팅
     private DbOpenHelper mDbOpenHelper; //내부 DB 관리
@@ -116,26 +93,18 @@ public class Scan1 extends AppCompatActivity {
     private DatabaseReference mFirebaseDatabase;
     private FirebaseDatabase mFirebaseInstance;
     int fire_id;
-
+    LocationManager lm;
     @Override
     protected void onStart() {
         super.onStart();
-        final LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         //firebase 연결
         mFirebaseInstance = FirebaseDatabase.getInstance();
         //referencce 초기 설정("DataUsers/")
         mFirebaseDatabase = mFirebaseInstance.getReference("DataUsers");
-
-        locationRequest = new LocationRequest().setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
-                .setInterval(UPDATE_INTERVAL_MS)
-                .setFastestInterval(FASTEST_UPDATE_INTERVAL_MS);
-
-        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
-        builder.addLocationRequest(locationRequest);
-
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-
     }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -161,6 +130,11 @@ public class Scan1 extends AppCompatActivity {
                 new String[]{"data", "distance"},
                 new int[]{android.R.id.text1, android.R.id.text2});
         listView.setAdapter(simpleAdapter);
+
+        gps = new GpsInfo(Scan1.this);
+        gps.getLocation();
+        Log.d("123123",  "123123");
+
 
         // Scan 시작
         startScanning();
@@ -188,26 +162,12 @@ public class Scan1 extends AppCompatActivity {
         senddt_bt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                final LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-                if (Build.VERSION.SDK_INT >= 23 &&
-                        ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                    ActivityCompat.requestPermissions(Scan1.this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
-                            0);
-                } else {
-                    Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-                    lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
-                            1000,
-                            1,
-                            gpsLocationListener);
-                    String provider = location.getProvider();
-                    longitude = location.getLongitude();
-                    latitude = location.getLatitude();
-                    Log.d("그치만,,,,", "위치정보 : " + provider + "\n" +
-                            "위도 : " + longitude + "\n" +
-                            "경도 : " + latitude + "\n");
-                }
-
+                latitude = gps.getLatitude();
+                longitude = gps.getLongitude();
+                System.out.println(latitude + longitude);
                 for (String key : sosDistance.keySet()) {
+                    latitude = gps.getLatitude();
+                    longitude = gps.getLongitude();
                     addSOSList(Integer.parseInt(key), sosDistance.get(key), latitude, longitude, sosData.get(key));
                 }
 
@@ -223,6 +183,31 @@ public class Scan1 extends AppCompatActivity {
 
             }
         });
+
+        //위치 관련
+        if (!isPermission) {
+            callPermission();
+            return;
+        }
+
+
+        // GPS 사용유무 가져오기
+        if (gps.isGetLocation()) {
+
+            latitude = gps.getLatitude();
+            longitude = gps.getLongitude();
+
+
+            Toast.makeText(
+                    getApplicationContext(),
+                    "당신의 위치 - \n위도: " + latitude + "\n경도: " + longitude,
+                    Toast.LENGTH_LONG).show();
+        } else {
+            // GPS 를 사용할수 없으므로
+            gps.showSettingsAlert();
+        }
+
+
 
     }
 
@@ -262,6 +247,7 @@ public class Scan1 extends AppCompatActivity {
                 }
 
                 testView.setText("RSSI 개수 : " + String.valueOf(rssiList.size()));
+                progress_bar(rssiList.size()*5);
 
             } else {
                 inputData = new HashMap<String, String>();
@@ -362,7 +348,6 @@ public class Scan1 extends AppCompatActivity {
             mean = mean + kalmanGain * (value - mean);
             variance = variance - (kalmanGain * variance);
         }
-
         return mean;
     }
 
@@ -406,265 +391,67 @@ public class Scan1 extends AppCompatActivity {
         mFirebaseDatabase.child("sos_info").child(String.valueOf(SOS_id)).child(String.valueOf(fire_id)).child("sos_content").setValue(sosData);
     }
 
-    final LocationListener gpsLocationListener = new LocationListener() {
-        public void onLocationChanged(Location location) {
 
-            String provider = location.getProvider();
-            longitude = location.getLongitude();
-            latitude = location.getLatitude();
-        }
-
-        public void onStatusChanged(String provider, int status, Bundle extras) {
-        }
-
-        public void onProviderEnabled(String provider) {
-        }
-
-        public void onProviderDisabled(String provider) {
-        }
-    };
-
-
-    //광규코드
-    public String getCurrentAddress(LatLng latlng) {
-
-        //지오코더... GPS를 주소로 변환
-        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
-
-        List<Address> addresses;
-
+    public void progress_bar(int value){
         try {
-            addresses = geocoder.getFromLocation(
-                    latitude = latlng.latitude,
-                    longitude = latlng.longitude,
-                    1);
-        } catch (IOException ioException) {
-            //네트워크 문제
-            Toast.makeText(this, "지오코더 서비스 사용불가", Toast.LENGTH_LONG).show();
-            return "지오코더 서비스 사용불가";
-        } catch (IllegalArgumentException illegalArgumentException) {
-            Toast.makeText(this, "잘못된 GPS 좌표", Toast.LENGTH_LONG).show();
-            return "잘못된 GPS 좌표";
 
-        }
+            // 변환된 값을 프로그레스바에 적용.
+            ProgressBar progress = (ProgressBar) findViewById(R.id.progress) ;
+            progress.setProgress(value) ;
 
-        if (addresses == null || addresses.size() == 0) {
-            Toast.makeText(this, "주소 미발견", Toast.LENGTH_LONG).show();
-            return "주소 미발견";
-
-        } else {
-            Address address = addresses.get(0);
-            return address.getAddressLine(0).toString();
+        } catch (Exception e) {
+            // 토스트(Toast) 메시지 표시.
+            Toast toast = Toast.makeText(Scan1.this, "Invalid number format",
+                    Toast.LENGTH_SHORT);
+            toast.show();
         }
 
     }
-
-    public boolean checkLocationServicesStatus() {
-        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-
-        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
-                || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
-    }
-
-
-    public void setCurrentLocation(Location location, String markerTitle, String markerSnippet) {
-
-        LatLng currentLatLng = new LatLng(location.getLatitude(), location.getLongitude());
-        //LatLng surLocation = new LatLng();//조난자 위치(삼각층량으로 얻어야함)
-
-//        MarkerOptions markerOptions = new MarkerOptions();
-//        markerOptions.position(currentLatLng);
-//        markerOptions.title(markerTitle);
-//        markerOptions.snippet(markerSnippet);
-//        markerOptions.draggable(true);
-//
-//        CircleOptions circle100m = new CircleOptions().center(currentLatLng)
-//                .radius(100)//100m
-//                .strokeWidth(0f)//선너비 0f : 선없음
-//                .fillColor(Color.parseColor("#100000FF"));//배경색
-//        mMap.addCircle(circle100m);
-//
-//
-//        currentMarker = mMap.addMarker(markerOptions);
-//
-//        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLng(currentLatLng);
-//        mMap.moveCamera(cameraUpdate);
-//        mMap.animateCamera(CameraUpdateFactory.zoomTo(17.0f));
-
-    }
-
-    public void onRequestPermissionsResult(int permsRequestCode,
-                                           @NonNull String[] permissions,
-                                           @NonNull int[] grandResults) {
-
-        if (permsRequestCode == PERMISSIONS_REQUEST_CODE && grandResults.length == REQUIRED_PERMISSIONS.length) {
-
-            // 요청 코드가 PERMISSIONS_REQUEST_CODE 이고, 요청한 퍼미션 개수만큼 수신되었다면
-
-            boolean check_result = true;
-
-
-            // 모든 퍼미션을 허용했는지 체크합니다.
-
-            for (int result : grandResults) {
-                if (result != PackageManager.PERMISSION_GRANTED) {
-                    check_result = false;
-                    break;
-                }
-            }
-
-
-            if (check_result) {
-
-                // 퍼미션을 허용했다면 위치 업데이트를 시작
-                startLocationUpdates();
-            } else {
-                // 거부한 퍼미션이 있다면 앱을 사용할 수 없는 이유를 설명해주고 앱을 종료.2 가지 경우
-
-                if (ActivityCompat.shouldShowRequestPermissionRationale(this, REQUIRED_PERMISSIONS[0])
-                        || ActivityCompat.shouldShowRequestPermissionRationale(this, REQUIRED_PERMISSIONS[1])) {
-
-
-                    // 사용자가 거부만 선택한 경우에는 앱을 다시 실행하여 허용을 선택하면 앱을 사용가능
-//                Snackbar.make(mLayout, "퍼미션이 거부되었습니다. 앱을 다시 실행하여 퍼미션을 허용해주세요. ",
-//                        Snackbar.LENGTH_INDEFINITE).setAction("확인", new View.OnClickListener() {
-//
-//                    @Override
-//                    public void onClick(View view) {
-//
-//                        finish();
-//                    }
-//                }).show();
-
-                } else {
-
-
-//                // "다시 묻지 않음"을 사용자가 체크하고 거부를 선택한 경우에는 설정(앱 정보)에서 퍼미션을 허용해야 앱을 사용가능
-//                Snackbar.make(mLayout, "퍼미션이 거부되었습니다. 설정(앱 정보)에서 퍼미션을 허용해야 합니다. ",
-//                        Snackbar.LENGTH_INDEFINITE).setAction("확인", new View.OnClickListener() {
-//
-//                    @Override
-//                    public void onClick(View view) {
-//
-//                        finish();
-//                    }
-//                }).show();
-                }
-            }
-
-        }
-    }
-
-    //여기부터는 GPS 활성화를 위한 메소드들
-    private void showDialogForLocationServiceSetting() {
-
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("위치 서비스 비활성화");
-        builder.setMessage("앱을 사용하기 위해서는 위치 서비스가 필요합니다.\n"
-                + "위치 설정을 수정하실래요?");
-        builder.setCancelable(true);
-        builder.setPositiveButton("설정", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int id) {
-                Intent callGPSSettingIntent
-                        = new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                startActivityForResult(callGPSSettingIntent, GPS_ENABLE_REQUEST_CODE);
-            }
-        });
-        builder.setNegativeButton("취소", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int id) {
-                dialog.cancel();
-            }
-        });
-        builder.create().show();
-    }
-
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
+    public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                                           int[] grantResults) {
+        if (requestCode == PERMISSIONS_ACCESS_FINE_LOCATION
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
 
-        switch (requestCode) {
+            isAccessFineLocation = true;
 
-            case GPS_ENABLE_REQUEST_CODE:
+        } else if (requestCode == PERMISSIONS_ACCESS_COARSE_LOCATION
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED){
 
-                //사용자가 GPS 활성 시켰는지 검사
-                if (checkLocationServicesStatus()) {
-                    if (checkLocationServicesStatus()) {
+            isAccessCoarseLocation = true;
+        }
 
-                        Log.d("조까", "onActivityResult : GPS 활성화 되있음");
-
-                        needRequest = true;
-
-                        return;
-                    }
-                }
-
-                break;
+        if (isAccessFineLocation && isAccessCoarseLocation) {
+            isPermission = true;
         }
     }
 
-    private void startLocationUpdates() {
+    // 전화번호 권한 요청
+    private void callPermission() {
+        // Check the SDK version and whether the permission is already granted or not.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                && checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
 
-        if (!checkLocationServicesStatus()) {
+            requestPermissions(
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_ACCESS_FINE_LOCATION);
 
-            Log.d(TAG, "startLocationUpdates : call showDialogForLocationServiceSetting");
-            showDialogForLocationServiceSetting();
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED){
+
+            requestPermissions(
+                    new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                    PERMISSIONS_ACCESS_COARSE_LOCATION);
         } else {
-
-            int hasFineLocationPermission = ContextCompat.checkSelfPermission(this,
-                    Manifest.permission.ACCESS_FINE_LOCATION);
-            int hasCoarseLocationPermission = ContextCompat.checkSelfPermission(this,
-                    Manifest.permission.ACCESS_COARSE_LOCATION);
-
-
-            if (hasFineLocationPermission != PackageManager.PERMISSION_GRANTED ||
-                    hasCoarseLocationPermission != PackageManager.PERMISSION_GRANTED) {
-
-                Log.d(TAG, "startLocationUpdates : 퍼미션 안가지고 있음");
-                return;
-            }
-
-
-            Log.d(TAG, "startLocationUpdates : call mFusedLocationClient.requestLocationUpdates");
-
-            mFusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper());
-
+            isPermission = true;
         }
-
     }
 
-    LocationCallback locationCallback = new LocationCallback() {
-        @Override
-        public void onLocationResult(LocationResult locationResult) {
-            super.onLocationResult(locationResult);
-
-            List<Location> locationList = locationResult.getLocations();
-
-            if (locationList.size() > 0) {
-                location = locationList.get(locationList.size() - 1);
-                //location = locationList.get(0);
-
-                currentPosition
-                        = new LatLng(location.getLatitude(), location.getLongitude());
 
 
-                String markerTitle = getCurrentAddress(currentPosition);
-                String markerSnippet = "위도:" + String.valueOf(location.getLatitude())
-                        + " 경도:" + String.valueOf(location.getLongitude());
-                latitude = location.getLatitude();
-                longitude = location.getLongitude();
-                Log.d(TAG, "onLocationResult : " + markerSnippet);
 
 
-                //현재 위치에 마커 생성하고 이동
-                setCurrentLocation(location, markerTitle, markerSnippet);
-
-                mCurrentLocatiion = location;
-            }
-
-
-        }
-    };
 }
